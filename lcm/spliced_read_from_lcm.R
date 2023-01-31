7cb7d11878d7752be1dcdbe9fb6ab7aba4a34f8d
NimraAslam23 <nimraaslam@hotmail.co.uk>
2023-01-31 19:17:57 +0000

    junc_cat, mean_n_dataset, plots

--- lcm/spliced_read_from_lcm.R
+++ lcm/spliced_read_from_lcm.R
@@ -8,7 +8,6 @@ library(dplyr)
 # Data import -------------------------------------------------------------
 
 spliced_reads_orig <- read.csv("spliced_read_from_lcm.csv")
-View(spliced_reads_orig)
 spliced_reads <- spliced_reads_orig
 
 # Looking at data ---------------------------------------------------------
@@ -26,70 +25,93 @@ group_column <- spliced_reads |>
                           "ALS",
                           "control"))
 
-View(group_column)
+# Density plot - number of spliced reads in ALS and controls --------------
 
-group_column |>  filter(grepl("STMN2",junction_name)) |> 
+group_column |> 
   ggplot(aes(x = n_spliced_reads,
              fill = disease)) +
   stat_density() + 
-  scale_x_continuous(trans = scales::pseudo_log_trans())   
+  scale_x_continuous(trans = scales::pseudo_log_trans()) +
+  labs(
+    title = "Number of spliced reads in ALS and controls",
+    x = "Number of spliced reads",
+    color = "Group",
+  )
 
-STMN2 <- group_column |>  filter(grepl("STMN2",junction_name)) 
-
-k <- wilcox.test(n_spliced_reads ~ disease, STMN2) |> 
-  broom::tidy()
-
-View(k)
-
-# Create a table of the average count of each junction in each disease
+# Table of the average count of each junction in each disease -------------
 
 mean_per_junction <- group_column |> 
   group_by(junction_name,disease) |> 
   mutate(mean_n_spliced_reads = mean(n_spliced_reads)) |> 
   ungroup() |> 
   select(junction_name,disease,mean_n_spliced_reads) |> 
-  unique() |> #have mean_n_spliced_reads for each junction_name separately for control and ALS
+  unique() |> #mean_n_spliced_reads for each junction_name separately for control and ALS
   pivot_wider(names_from = 'disease',
               values_from = 'mean_n_spliced_reads')
 
-# Nest data by junction_name
+# Nest data by junction_name ----------------------------------------------
 
 group_column_nested <- group_column |> 
   group_by(junction_name) |> 
   nest()
-  
-View(group_column_nested)
 
-# Run wilcoxon test on all the nested data
+# Run wilcoxon test on all the nested data --------------------------------
 
 wilcox_tested_nested = group_column_nested |> 
   mutate(wc_res = map(data,~{broom::tidy(wilcox.test(.x$n_spliced_reads ~ .x$disease, exact=FALSE))})) |> 
   unnest(wc_res) |> 
   arrange(p.value)
 
-View(wilcox_tested_nested)
-
-# Find the junctions which were significant (p < 0.05)
+# Find the junctions which were significant (p < 0.05) --------------------
 
 significant_junctions <- wilcox_tested_nested |> 
-  filter(p.value < 0.05) 
+  filter(p.value < 0.05) # 33 significant junctions
 
-View(significant_junctions) # 33 significant junctions
-
-# What fraction of the significant junctions are more expressed in ALS? More expressed in controls?
+# Fraction of significant junctions expressed more in ALS and cont --------
 
 mean_per_junction_sig <- mean_per_junction |> 
-  semi_join(significant_junctions, mean_per_junction, by=("junction_name"))
+  semi_join(significant_junctions, by=("junction_name")) |> 
+  mutate(higher_value = ifelse(ALS>control, "ALS", "control")) |> 
+  janitor::tabyl(higher_value)
 
-control_sig <- mean_per_junction_sig |> 
-  filter(control > ALS)
+# Number of significant spliced reads in ALS and controls -----------------
 
-ALS_sig <- mean_per_junction_sig |> 
-  filter(ALS > control)
+sig_reads_groups <- group_column |> 
+  semi_join(significant_junctions, by=("junction_name")) 
 
-  #fraction more expressed in controls
-fraction_control_sig <- nrow(control_sig)/nrow(significant_junctions)
+ggplot(sig_reads_groups, aes(x = n_spliced_reads, color = disease, fill = disease)) +
+  geom_density(alpha = 0.5) +
+  scale_x_continuous(trans = scales::pseudo_log_trans()) +
+  labs(
+    title = "Number of significant spliced reads in ALS and controls",
+    x = "Number of spliced reads",
+  )
 
-  #fraction more expressed in ALS
-fraction_ALS_sig <- nrow(ALS_sig)/nrow(significant_junctions)
+# Junctions higher in the controls vs ALS ---------------------------------
 
+mean_per_junction |> 
+  semi_join(significant_junctions, by=("junction_name")) |> 
+  mutate(higher_value = ifelse(ALS>control,"ALS","control")) |> 
+  separate(junction_name, sep = '\\|',into = c("gene","junc_cat","n_datasets_junction_found"),convert = TRUE) |>
+  janitor::tabyl(junc_cat,higher_value)
+
+mean_per_junction |> 
+  semi_join(significant_junctions, by=("junction_name")) |> 
+  mutate(higher_value = ifelse(ALS>control,"ALS","control")) |> 
+  separate(junction_name, sep = '\\|',into = c("gene","junc_cat","n_datasets_junction_found"),convert = TRUE) |> 
+  ggplot(aes(x = junc_cat, fill = higher_value)) +
+  geom_bar() +
+  labs(
+    title = "Number of junctions significantly higher in ALS and controls",
+    x = "Junction category",
+    y = "Number of junctions",
+    color = "Group",
+  )
+
+# Mean 'n_datasets_junction_found' by which tissue it was higher in -------
+
+mean_per_junction |> 
+  semi_join(significant_junctions, by=("junction_name")) |> 
+  mutate(higher_value = ifelse(ALS>control,"ALS","control")) |> group_by(higher_value) |> 
+  separate(junction_name, sep = '\\|',into = c("gene","junc_cat","n_datasets_junction_found"),convert = TRUE) |> 
+  summarize(mean_n_datasets = mean(n_datasets_junction_found))
