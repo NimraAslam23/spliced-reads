eebe4bfb4bdab05305e6875f8466d7061bab132d
NimraAslam23 <nimraaslam@hotmail.co.uk>
2023-06-19 13:42:06 +0100

    read in data for missing patient mutation data
    tidying tables too

--- lcm/mutations.R
+++ lcm/mutations.R
@@ -7,16 +7,13 @@ library(repurrrsive)
 library(jsonlite)
 library(clipr)
 library(naniar)
-library(rstatix)
 library(knitr)
 library(snapcount)
 library(ggsignif)
-library(GenomicDataCommons)
+library(TCGAbiolinks)
+library(stringr)
 
-if (!require("BiocManager", quietly = TRUE, force=TRUE))
-  install.packages("BiocManager")
-
-BiocManager::install("TCGAbiolinks", force=TRUE)
+# need all_common_cases df from the combined.R script
 
 # pull back all mutation data from TCGA -----------------------------------
 
@@ -55,20 +52,73 @@ write.table(maf, file='\\Users\\nimraaslam\\Documents\\GitHub\\spliced-reads\\lc
 write.table(maf, file='all_mutation_data.txt')
 
 
-all_mutation_data_orig <- data.frame(maf)
-all_mutation_data <- data.frame(maf)
+all_mutation_data_orig <- read.table("all_mutation_data.txt", sep="", header = T)
+all_mutation_data <- all_mutation_data_orig
 
 # left join clinical df to mutation df ------------------------------------
 
 all_mutation_data <- all_mutation_data |> 
-  mutate(case_submitter_id = str_extract(Tumor_Sample_Barcode, "([^-]+-[^-]+-[^-]+)")) |> 
-  select(Hugo_Symbol, Entrez_Gene_Id, Chromosome, Start_Position, End_Position, Strand, Variant_Classification, Variant_Type, Reference_Allele, Tumor_Seq_Allele1,
-         Tumor_Seq_Allele2, dbSNP_RS, dbSNP_Val_Status, Tumor_Sample_Barcode, Mutation_Status, HGVSc, HGVSp, HGVSp_Short, Transcript_ID, Exon_Number, all_effects,
-         Allele, Gene, One_Consequence, Consequence, Amino_acids, SYMBOL, BIOTYPE, SIFT, PolyPhen, IMPACT, VARIANT_CLASS, case_submitter_id)
+  janitor::clean_names() |> 
+  mutate(case_submitter_id = str_extract(tumor_sample_barcode, "([^-]+-[^-]+-[^-]+)")) |> 
+  relocate(case_submitter_id, .before="x1")
 
 mutation_clinical_data <- all_mutation_data |> 
+  dplyr::select(hugo_symbol, variant_classification,
+         variant_type, reference_allele, tumor_seq_allele2, db_snp_rs, 
+        hgv_sp_short, poly_phen, impact, case_submitter_id) |> 
   left_join(cBio_clinical, by="case_submitter_id") |> 
-  janitor::clean_names()
+  janitor::clean_names() |> 
+  dplyr::select(-c(sample_id, cancer_type)) |> 
+  rename("gene" = "hugo_symbol",
+         "variant_allele" = "tumor_seq_allele2",
+         "db_snp" = "db_snp_rs") |> 
+  mutate(protein_change = str_remove(hgv_sp_short, "^p\\.")) |> 
+  select(-hgv_sp_short)
+
+# filter for patients with >1 cryptic event -------------------------------
+
+all_common_cases <- intersect(mutation_clinical_data$case_submitter_id, all_common_cases$case_submitter_id)
+
+mutation_clinical_multiple_cryptic_events <- mutation_clinical_data |> 
+  dplyr::filter(case_submitter_id %in% all_common_cases) 
 
 
+# read in data for missing patient mutation data --------------------------
 
+read_save_mutation_data <- function(filename) {
+  df <- janitor::clean_names(read.csv(filename, sep = "\t"))
+  return(df)
+}
+# read_save_mutation_data("TCGA-VQ-A91N_mutations.tsv") |> View()
+
+combine_mutation_data <- function(folder_path, pattern = "^TCGA.*_mutations.tsv$") {
+  mutation_files <- list.files(folder_path,
+                               pattern = pattern,
+                               full.names = TRUE)
+  #print(mutation_files)
+  df <- purrr::map(mutation_files, read_save_mutation_data)
+  patient_id <- purrr::simplify(purrr::map(mutation_files, basename))
+  patient_id = gsub("_mutations.tsv", "", patient_id)
+  non_empty_df <- df[sapply(df, function(x) nrow(x) > 0)]
+  non_empty_patient_id <- patient_id[sapply(df, function(x) nrow(x) > 0)]
+  df <- purrr::map2(non_empty_df, non_empty_patient_id, ~cbind(.x, case_submitter_id = .y))
+  df <- data.table::rbindlist(df)
+  return(df)
+}
+
+combined_mutation_data <- combine_mutation_data('/Users/nimraaslam/Documents/GitHub/spliced-reads/lcm') |> 
+  dplyr::select(-c(gene_panel, annotation, chromosome, start_pos, end_pos, hgv_sg, ms, vs, center, allele_freq, variant_reads, 
+            ref_reads, variant_reads_normal, ref_reads_normal, copy, cosmic, exon, gnom_ad, clin_var, signal)) |> 
+  left_join(cBio_clinical, by="case_submitter_id") |> 
+  janitor::clean_names() |> 
+  select(-ends_with("_y"), -cancer_type, -sample_id, -hgv_sc) |> 
+  separate(functional_impact, into = c("impact", "sift", "poly_phen"), sep = ";") |> 
+  select(-sift) |> 
+  rename("variant_classification" = "mutation_type",
+         "reference_allele" = "ref",
+         "variant_allele" = "var") |> 
+  select(colnames(mutation_clinical_data))
+  
+
+
+  
